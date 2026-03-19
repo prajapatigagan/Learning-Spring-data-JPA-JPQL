7150466c4050d05071147ee1cc7960c84fb7484e
prajapatigagan <gaganprajapati121@gmail.com>
2026-03-19 23:13:25 +0530

    next commit

---
+++ src/main/java/com/gagan/Hospatil_Management_System/entity/Insurance.java
@@ -0,0 +1,37 @@
+package com.gagan.Hospatil_Management_System.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Insurance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true,length = 50)
+    private String policyNumber;
+
+    @Column(nullable = false,length = 100)
+    private String provider;
+
+    @Column(nullable = false)
+    private LocalDate velidUntil;
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+}
